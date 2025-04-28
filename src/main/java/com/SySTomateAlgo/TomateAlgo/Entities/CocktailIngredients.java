fb2d04d37e49b8f9a907194c648cb4c1cff4529a
Fran3103 <fn.aguirre@hotmail.com>
2025-04-28 11:23:33 -0300

    agregando entidad Ingredientes

---
+++ src/main/java/com/SySTomateAlgo/TomateAlgo/Entities/CocktailIngredients.java
@@ -0,0 +1,72 @@
+package com.SySTomateAlgo.TomateAlgo.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cocktail_ingredient")
+public class CocktailIngredients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cocktail_id", nullable = false)
+    private Cocktail cocktail;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "ounces", nullable = false)
+    private Double ounces;
+
+    public CocktailIngredients() {
+    }
+
+    public CocktailIngredients(Double ounces, Product product, Cocktail cocktail) {
+        this.ounces = ounces;
+        this.product = product;
+        this.cocktail = cocktail;
+    }
+
+    public CocktailIngredients(Long id, Cocktail cocktail, Product product, Double ounces) {
+        this.id = id;
+        this.cocktail = cocktail;
+        this.product = product;
+        this.ounces = ounces;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cocktail getCocktail() {
+        return cocktail;
+    }
+
+    public void setCocktail(Cocktail cocktail) {
+        this.cocktail = cocktail;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Double getOunces() {
+        return ounces;
+    }
+
+    public void setOunces(Double ounces) {
+        this.ounces = ounces;
+    }
+}
