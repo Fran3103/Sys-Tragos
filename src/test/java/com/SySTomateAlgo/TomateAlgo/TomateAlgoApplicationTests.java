a4d72258467ab6f9ef37ff5cce5245a138e84a71
Fran3103 <fn.aguirre@hotmail.com>
2025-05-01 13:01:15 -0300

    configuracion de test y db

--- src/test/java/com/SySTomateAlgo/TomateAlgo/TomateAlgoApplicationTests.java
+++ src/test/java/com/SySTomateAlgo/TomateAlgo/TomateAlgoApplicationTests.java
@@ -2,8 +2,10 @@ package com.SySTomateAlgo.TomateAlgo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TomateAlgoApplicationTests {
 
 	@Test
